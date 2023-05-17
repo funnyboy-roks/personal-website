145934b8d9e0aeec90a777ffabca58afe178003c
funnyboy-roks <funnyboyroks@gmail.com>
2023-05-17 12:24:59 -0500

    Add "Under Construction"

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,4 @@
 <div>
-    Hello World
+    <h1>This page is currently under construction!</h1>
+    <h3>You can view all of my contact information on my <a href="https://github.com/funnyboy-roks#contact">GitHub</a></h3>
 </div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<div>
+    <h1>This page is currently under construction!</h1>
+    <h3>You can view all of my projects on my <a href="https://github.com/funnyboy-roks">GitHub</a></h3>
+</div>
