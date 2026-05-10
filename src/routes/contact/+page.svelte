df17321b7f329d38ba2a339ec9c13dd54a5ab367
funnyboy-roks <fbr@fbr.dev>
2026-05-10 10:14:39 -0500

    add proper quoting gimmick to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,45 @@
 <script lang="ts">
-	import { browser } from "$app/environment";
 	import type { PageProps } from "./$types";
     const email = 'me@haydenpott.com';
 
     let { data, form }: PageProps = $props();
+
+    let name = $derived(form?.name || '');
+    let contact = $derived(form?.contact || '');
+    let content = $derived(form?.content || '');
+
+    const quote = (content: string | undefined): [string, string, number] => {
+        if (!content) return ['r"', '"', 0];
+        let matches = content.match(/"#*/g);
+        if (!matches) return ['r"', '"', 0];
+        let len = matches.map(m => m.length)
+            .reduce((a, b) => Math.max(a, b), 1);
+        let hashes = '#'.repeat(len);
+        return [`r${hashes}"`, `"${hashes}`, len];
+    };
+
+    let quotes = $derived.by(() => {
+        return {
+            name: quote(name),
+            contact: quote(contact),
+            content: quote(content),
+        };
+    });
+
+    let spaces = $derived.by(() => {
+        const name_len = quote(name)[2];
+        const contact_len = quote(contact)[2];
+        const content_len = quote(content)[2];
+        const max_len = Math.max(name_len, contact_len, content_len);
+
+        const nbsp = '\u00a0';
+        return {
+            name: nbsp.repeat(max_len - name_len),
+            contact: nbsp.repeat(max_len - contact_len),
+            content: nbsp.repeat(max_len - content_len),
+        };
+    });
+
 </script>
 
 <div class="flex flex-col gap-4 container">
@@ -38,26 +74,24 @@
                 {#if form?.error === 'missing-fields' && form.missingFields.includes('name')}
                     &nbsp;&nbsp;&nbsp;&nbsp;<span class="b8 error ">/// Required Field</span> <br />
                 {/if}
-                &nbsp;&nbsp;&nbsp;&nbsp;name:&nbsp;&nbsp;&nbsp;&nbsp;
-                    <span class="bb">"</span
-                    ><input type="text" autocomplete="name" class="bb" name="name" value={form?.name || ''} placeholder="Enter name"
-                    /><span class="bb">"</span>, <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;name:&nbsp;&nbsp;&nbsp;&nbsp;{spaces.name}<span class="bb">{quotes.name[0]}</span
+                    ><input type="text" autocomplete="name" class="bb" name="name" bind:value={name} placeholder="Enter name"
+                    /><span class="bb">{quotes.name[1]}</span>, <br />
                 {#if form?.error === 'missing-fields' && form.missingFields.includes('contact')}
                     &nbsp;&nbsp;&nbsp;&nbsp;<span class="b8 error">/// Required Field</span> <br />
                 {/if}
                 {#if form?.error === 'invalid-contact'}
                     &nbsp;&nbsp;&nbsp;&nbsp;<span class="b8 error ">/// Contact may not be '{form.contact}'.</span> <br />
                 {/if}
-                &nbsp;&nbsp;&nbsp;&nbsp;contact:&nbsp;
-                    <span class="bb">"</span
-                    ><input type="text" autocomplete="email impp" class="bb" name="contact" value={form?.contact || ''} placeholder="Enter contact info"
-                    /><span class="bb">"</span>, <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;contact: {spaces.contact}<span class="bb">{quotes.contact[0]}</span
+                    ><input type="text" autocomplete="email impp" class="bb" name="contact" bind:value={contact} placeholder="Enter contact info"
+                    /><span class="bb">{quotes.contact[1]}</span>, <br />
                 {#if form?.error === 'missing-fields' && form.missingFields.includes('content')}
                     &nbsp;&nbsp;&nbsp;&nbsp;<span class="b8 error">/// Required Field</span> <br />
                 {/if}
-                &nbsp;&nbsp;&nbsp;&nbsp;content: <span class="bb">r#"</span> <br />
-                    &nbsp;&nbsp;&nbsp;&nbsp;<textarea cols="42" rows="4" name="content" value={form?.content || ''}></textarea> <br />
-                    &nbsp;&nbsp;&nbsp;&nbsp;<span class="bb">"#</span>, <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;content: {spaces.content}<span class="bb">{quotes.content[0]}</span> <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;<textarea cols="42" rows="4" name="content" bind:value={content}></textarea> <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;<span class="bb">{quotes.content[1]}</span>, <br />
                     &rbrace;;<br />
                 msg.<span class="bd"><button id="submit" type="submit">send</button></span>()<span class="b9">?</span>; <span class="b4">// Click `send` to send message</span>
             </pre>
